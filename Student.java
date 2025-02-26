8b548f4663789b1611a0182c65836b4d6edb61cf
Howexou <l.cartagena1221@gmail.com>
2025-02-26 23:48:17 +0100

    OUAI

--- Student.java
+++ Student.java
@@ -6,4 +6,11 @@ class Student {
     private String studentID;
     private int age;
     private List<Double> grades;
+
+    public Student(String name, String studentID, int age) {
+        this.name = name;
+        this.studentID = studentID;
+        this.age = age;
+        this.grades = new ArrayList<>();
+    }
 }
