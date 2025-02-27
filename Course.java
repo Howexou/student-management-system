c76ff984e0d3ee69cc420e25269fa0d2f9f4ef22
Howexou <l.cartagena1221@gmail.com>
2025-02-27 01:01:05 +0100

    course

--- Course.java
+++ Course.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Course {
     private String courseName;
     private String courseCode;
@@ -11,5 +14,42 @@ class Course {
         this.students = new ArrayList<>();
     }
 
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public void setCourseCode(String courseCode) {
+        this.courseCode = courseCode;
+    }
+
+    public int getCreditHours() {
+        return creditHours;
+    }
+
+    public void setCreditHours(int creditHours) {
+        this.creditHours = creditHours;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void enrollStudent(Student student) {
+        students.add(student);
+    }
+
     
 }
+    
